bfdd7fd1242877f56e4660ca57f2fee788cc4a45
Matthew Elam <ematew418@gmail.com>
2022-08-09 13:06:39 -0400

    commit tree 3e26f199709b7141d326518a6bd7d8d5888ffa86

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return 8
+}
+
+const distancefromHqinFeet = function distancefromHqinFeet() {
+    return 256
+}
+
